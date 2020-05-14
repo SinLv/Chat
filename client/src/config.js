dd643da2790f49462ca0066f48b61c8288a14a31
Lv Xi <lvxi1014@outlook.com>
2020-05-14 21:42:53 +0800

    用户登录页和用户列表页

--- client/src/config.js
+++ client/src/config.js
@@ -3,11 +3,11 @@ import {Toast} from 'antd-mobile'
 
 //拦截请求
 axios.interceptors.request.use(function(config){
-    Toast.loading('加载中',0)
+    // Toast.loading('加载中',0)
     return config
 })
 //拦截响应
 axios.interceptors.response.use(function(config){
-    Toast.hide('加载中',0)
+    // Toast.hide('加载中',0)
     return config
 })
--- server/src/routers.js
+++ server/src/routers.js
@@ -4,6 +4,7 @@ const utils = require('utility')
 const model = require('./model');
 const User = model.getModel('user');
 const Chat = model.getModel('chat');
+const _filter = {'pwd':0,'__v':0}
 
 routes.post('/user/register', async (ctx, next) => {
     const {user,pwd,type} = ctx.request.body;
@@ -20,11 +21,35 @@ routes.post('/user/register', async (ctx, next) => {
         ctx.response.body = {code: 0, data:{user: data.user,type: data.type,_id: data._id}};
     }
     next();
-}).post('/user/login', (ctx, next) => {
-    const {user, pwd} = ctx.request.body;
+}).post('/user/update', async (ctx, next) => {
+    const userid = ctx.cookies.get('userid');
+    if (!userid) { // 说明还没有注册
+        ctx.response.body = {code: 1};
+    }
+    let result = await User.findByIdAndUpdate(userid, ctx.request.body);
+    console.log(result);
+    const data = Object.assign({}, {
+        user: result.user,
+        type: result.type,
+    }, ctx.request.body);
+    ctx.response.body = {code: 0, data};
     next();
-}).get('/user/list', (cxt, next) => {
-    const type = cxt.request.query;
+}).post('/user/login', async (ctx, next) => {
+    const {user, pwd} = ctx.request.body;
+    let result = await User.findOne({user: user}, _filter);
+    console.log(result);
+    if (!result) {
+        ctx.response.body = {code: 1, msg:'用户名或者密码错误'};
+    } else {
+        ctx.cookies.set('userid', result._id);
+        ctx.response.body = {code: 0, data:result};
+    }
+    next();
+}).get('/user/list', async (cxt, next) => {
+    const {type} = cxt.request.query;
+    console.log(type);
+    let result = await User.find({type});
+    cxt.response.body = {code: 0, data: result};
     next();
 }).get('/user/info', (ctx, next) => {
     const userid = ctx.cookies.get('userid');
